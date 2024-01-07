51d06420364942cca0c6eb1c514fea343abfb9bc
VoidKeishi <phananha4@gmail.com>
2024-01-07 21:49:41 +0700

    Update menu

--- sourcecode/src/gui/app/App.java
+++ sourcecode/src/gui/app/App.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -12,7 +13,9 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/gui/view/gui.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("TSP");
+        Image icon = new Image(getClass().getResourceAsStream("/gui/view/main.png"));
+        stage.getIcons().add(icon);
+        stage.setTitle("Genetic Algorithm Visualizer");
         stage.setScene(scene);
         stage.show();
     }
